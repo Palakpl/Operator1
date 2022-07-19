e919e8340ba26c125c29d255ddc7ef1efb44d7cc
Palak Panchal <pn_panchal28@yahoo.com>
2022-07-19 21:06:39 +0100

    added in arithmetic program

--- arithmetic1.java
+++ arithmetic1.java
@@ -4,13 +4,15 @@ public class arithmetic1 {
     public static void main(String[] args) {
         int a = 15;
         double b = 89.90;
-        double c;
+        double c,d;
         a++;
         --b;
         c = a * b ;
+        d = b/a;
         System.out.println("a = " + a);
         System.out.println("b = " + b);
         System.out.println("c = " + c);
+        System.out.println("d = " + d);
 
 
 
